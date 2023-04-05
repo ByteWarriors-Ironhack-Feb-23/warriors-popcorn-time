6c39a63e7d43813e392cd9117a0aaba1b39dfe16
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-04-05 12:43:12 +0200

    (lift state up) display number of movies in Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import moviesFromJson from "./data/movies.json";
+
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
+import './App.css';
+
 function App() {
+
+  const [moviesArr, setMoviesArr] = useState(moviesFromJson);
+
+  const deleteMovie = (movieId) => {
+    const newList = moviesArr.filter( movieDetails => movieDetails.id !== movieId );
+    setMoviesArr(newList);
+  }
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Header numberOfMovies={moviesArr.length} />
+      <Main moviesArr={moviesArr} callbackToDelete={deleteMovie} />
       <Footer />
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,49 +1,15 @@
 import { useState } from "react";
-import moviesFromJson from "../data/movies.json";
 import Movie from "./Movie";
 
 import "./Main.css"
 
-function Main(){
-
-    const [moviesArr, setMoviesArr] = useState(moviesFromJson);
-    
-
-
-    const deleteMovie = (movieId) => {
-
-        // const newList = moviesArr.filter( (movieDetails) => {
-        //     if(movieDetails.id === movieId){
-        //         return false; //this movie will not be included in the new array
-        //     } else {
-        //         return true;
-        //     }
-        // });
-
-        const newList = moviesArr.filter( movieDetails => movieDetails.id !== movieId );
-
-
-        setMoviesArr(newList);
-    }
-
-
-    let title;
-    if(moviesArr.length > 1){
-        title = <h1>We have {moviesArr.length} movies</h1>;
-    } else if(moviesArr.length === 1){
-        title = <h1>We have 1 movie</h1>;
-    } else {
-        title = <h1>Sorry, no movies to display</h1>;
-    }
-
+function Main(props){
 
     return(
         <div className="Main">
-            
-            {title}
 
-            {moviesArr.map( (movieObj) => {
-                return <Movie key={movieObj.id} movieDetails={movieObj} callbackToDelete={deleteMovie} />
+            {props.moviesArr.map( (movieObj) => {
+                return <Movie key={movieObj.id} movieDetails={movieObj} callbackToDelete={props.callbackToDelete}  />
             })}
 
         </div>
